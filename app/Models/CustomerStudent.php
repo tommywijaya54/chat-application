add2b0cfbb1e9c7b7c2fe25da5a5e767dd47fa99
tommywijaya54 <tommy.wijaya54@yahoo.com>
2022-09-13 15:01:16 +1000

    New Database for Learning center
    php artisan make:model CustomerParent -mc &&
    php artisan make:model CustomerStudent -mc &&
    php artisan make:model Teacher -mc &&
    php artisan make:model Advisor -mc &&
    php artisan make:model Employee -mc &&
    php artisan make:model Branch -mc

---
+++ app/Models/CustomerStudent.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class CustomerStudent extends Model
+{
+    use HasFactory;
+}
