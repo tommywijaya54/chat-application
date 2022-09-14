c5230af427ccac344fe96ff98f6abcf1b0e2ee41
tommywijaya54 <tommy.wijaya54@yahoo.com>
2022-09-14 15:49:17 +1000

    Create List view of Parents

--- app/Http/Controllers/CustomerParentController.php
+++ app/Http/Controllers/CustomerParentController.php
@@ -2,9 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\CustomerParent;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class CustomerParentController extends Controller
 {
     //
+    public function index()
+    {
+        return Inertia::render('Parent/Index', [
+            'parents' => CustomerParent::all()
+        ]);
+
+        /*
+            return Inertia::render('Parent/Index', [
+                'parents' => CustomerParent::all(),
+                'message' => 'List orang tua loaded'
+            ])->with('message', 'Contact created.');
+        */
+    }
 }
---
+++ resources/js/Pages/Parent/Index.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import MainAuthenticatedLayout from '@/Layouts/MainAuthenticatedLayout';
+import { InertiaLink } from '@inertiajs/inertia-react';
+import Icon from '@/Shared/Icon';
+
+const ParentList = ({List}) => {
+    return <>
+        <div className="overflow-x-auto bg-white rounded shadow">
+            <table className="w-full whitespace-nowrap table-padding-row"> 
+                    <thead className="text-left">
+                        <tr>
+                            <th>Name</th>
+                            <th>Phone</th>
+                            <th>NIK</th>
+                            <th>Email</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            List.map(({id, name, phone, nik, email}) => {
+                                return <tr 
+                                    key={id}
+                                    className="hover:bg-gray-100 focus-within:bg-gray-100"
+                                    >
+                                    <td>{name}</td>
+                                    <td>{phone}</td>
+                                    <td>{nik}</td>
+                                    <td>{email}</td>
+                                    <td>
+                                        <InertiaLink
+                                            tabIndex="-1"
+                                            href={route('parent.show', id)}
+                                            className="flex items-center px-4 focus:outline-none"
+                                        >
+                                        <Icon
+                                            name="cheveron-right"
+                                            className="block w-6 h-6 text-gray-400 fill-current"
+                                        />
+                                        </InertiaLink>
+                                    </td>
+                                </tr>
+                            })
+                        }
+                    </tbody>
+            </table>
+        </div>
+    </>
+}
+
+export default function ParentEdit(props) {
+    return (
+        <MainAuthenticatedLayout
+            auth={props.auth}
+            errors={props.errors}
+            title="Parent List"
+        >
+            <ParentList List={props.parents}></ParentList>
+        </MainAuthenticatedLayout>
+    );
+}
