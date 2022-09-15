d6825c63b4cf6cf37d03c997ef4f0ae07d585199
tommywijaya54 <tommy.wijaya54@yahoo.com>
2022-09-15 15:54:29 +1000

    Adding edit and update function on Student Controller

--- app/Http/Controllers/CustomerStudentController.php
+++ app/Http/Controllers/CustomerStudentController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\CustomerStudent;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redirect;
 use Inertia\Inertia;
 
 class CustomerStudentController extends Controller
@@ -13,5 +14,29 @@ class CustomerStudentController extends Controller
         return Inertia::render('Student/Index', [
             'students' => CustomerStudent::all()
         ]);
+    }
+
+    public function edit(CustomerStudent $student)
+    {
+        return Inertia::render('Student/Edit', [
+            'student' => $student,
+            'parent' => $student->parent
+        ]);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $entity = CustomerStudent::find($id);
+
+        $entity->name = $request->name;
+        $entity->school = $request->school;
+        $entity->grade = $request->grade;
+        $entity->phone = $request->phone;
+        $entity->email = $request->email;
+        $entity->note = $request->note;
+
+        $entity->update();
+
+        return Redirect::route('student.index')->with('message', 'Student details of ' . $entity->name . ' updated');
     }
 }
